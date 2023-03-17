342a9c62b8c498508509a5500fb830a01b0d8ea8
karthickb2210 <116095490+karthickb2210@users.noreply.github.com>
2023-03-17 06:28:14 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 
 const express = require("express");
 const app = express();
+const path = require('path')
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css/bootstrap.min.css')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js/bootstrap.min.js')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js/jqueory.js')))
 app.get("/",function(req,res){
     res.sendFile(__dirname+"/index.html");
 });
